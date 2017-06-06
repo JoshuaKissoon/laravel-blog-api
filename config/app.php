7f7d634396da257834c8d9a381d266bc978ededa
Joshua Kissoon <JoshuaKissoon@gmail.com>
2017-06-06 08:31:16 -0400

    Added the laravel-cors package (https://github.com/barryvdh/laravel-cors)
    Setup token based oAuth authentication using Laravel passport

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class, 
+        Laravel\Passport\PassportServiceProvider::class,
 
         /*
          * Application Service Providers...
