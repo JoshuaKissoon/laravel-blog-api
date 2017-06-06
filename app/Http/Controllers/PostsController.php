90eb7989d6694af7451fc3485f6465c8fd6d6c81
Joshua Kissoon <JoshuaKissoon@gmail.com>
2017-06-06 11:04:47 -0400

    Setting up the blogging API

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,6 +1,8 @@
 <?php
 
     namespace App\Http\Controllers;
+    
+    use Illuminate\Http\Request;
 
 
     class PostsController extends Controller
@@ -8,7 +10,7 @@
 
         public function __construct()
         {
-            $this->middleware("auth")->except(["index", "show"]);
+            $this->middleware("auth:api")->except(["show"]);
         }
 
         public function index()
@@ -17,13 +19,7 @@
                     ->filter(request(['month', 'year']))
                     ->get();
 
-            $archives = \App\Post::selectRaw('year(created_at) as year, monthname(created_at) as month, count(*) as published')
-                    ->groupBy('year', 'month')
-                    ->orderByRaw('MIN(created_at) DESC')
-                    ->get()
-                    ->toArray();
-
-            return view("posts.index", compact('posts', 'archives'));
+            return $posts;
         }
 
         public function show(\App\Post $post)
@@ -31,13 +27,8 @@
             return view("posts.show", compact("post"));
         }
 
-        public function create()
-        {
-            return view('posts.create');
-        }
-
         public function store()
-        {
+        {            
             $this->validate(request(), [
                 "title" => "required",
                 "body" => "required",
@@ -48,11 +39,9 @@
             $post->body = request('body');
             $post->user_id = auth()->id();
 
-
             $post->save();
-
-
-            return redirect("/");
+            
+            return $post;
         }
 
     }
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,19 @@
 <?php
 
-    Route::get("/", 'PostsController@index')->name("home");
-
-    Route::get("posts/create", 'PostsController@create');
-
-    Route::post("/posts", "PostsController@store");
-
-    Route::get("posts/{post}", 'PostsController@show');
-    
-    Route::post("posts/{post}/comments", 'CommentsController@store');
-    
-    Route::get('/register', "RegistrationController@create");
-    
-    Route::post('/register', "RegistrationController@store");
-    
-    Route::get('/login', "SessionsController@create");
-    Route::post('/login', "SessionsController@store");
-    Route::get('/logout', "SessionsController@destroy");
+//    Route::get("/", 'PostsController@index')->name("home");
+//
+//    Route::get("posts/create", 'PostsController@create');
+//
+//    Route::post("/posts", "PostsController@store");
+//
+//    Route::get("posts/{post}", 'PostsController@show');
+//    
+//    Route::post("posts/{post}/comments", 'CommentsController@store');
+//    
+//    Route::get('/register', "RegistrationController@create");
+//    
+//    Route::post('/register', "RegistrationController@store");
+//    
+//    Route::get('/login', "SessionsController@create");
+//    Route::post('/login', "SessionsController@store");
+//    Route::get('/logout', "SessionsController@destroy");
